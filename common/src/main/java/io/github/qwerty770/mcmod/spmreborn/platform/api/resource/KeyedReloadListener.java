c59dfb05d43d161e34e8223a679627564f82b098
Justin Zhang <2691475984@qq.com>
2023-12-16 23:53:55 +0800

    Fix: fabric.mod.json

--- common/src/main/java/io/github/qwerty770/mcmod/spmreborn/platform/api/resource/KeyedReloadListener.java
+++
@@ -1,9 +0,0 @@
-package io.github.qwerty770.mcmod.spmreborn.platform.api.resource;
-
-import net.minecraft.resources.ResourceLocation;
-
-// TODO
-@Deprecated
-public interface KeyedReloadListener extends InternalReloadListener {
-    ResourceLocation getId();
-}
